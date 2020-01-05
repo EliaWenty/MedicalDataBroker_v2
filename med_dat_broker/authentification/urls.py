f1921e307f78308435e4867412e36c0f6de3f266
eliwe <eliwen@gmx.at>
2020-01-05 23:07:41 +0100

    csv reader v1

--- med_dat_broker/authentification/urls.py
+++ med_dat_broker/authentification/urls.py
@@ -3,14 +3,7 @@ from . import views
 from physioNet import views as physioView
 
 
-#urlpatterns = [
-    #path('', views.home, name='authentification-home'),
-    #path('auth/', physioView.home, name='physioFromAuth')
-#]
-# urls.py
-from django.contrib import admin
-from django.urls import path
-
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', views.home, name='authentification-home'),
+    path('admin/', physioView.home, name='physioFromAuth')
 ]
