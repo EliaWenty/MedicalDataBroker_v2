bdc193d1c86ce12cb43119f73ed90265e65ce6ad
Sara Saric <SAR17661@spengergasse.at>
2020-01-05 23:10:08 +0100

    login auf django admin geändert

--- med_dat_broker/authentification/urls.py
+++ med_dat_broker/authentification/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 from physioNet import views as physioView
 
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='authentification-home'),
-    path('admin/', physioView.home, name='physioFromAuth')
+    path('admin/', admin.site.urls),
 ]
