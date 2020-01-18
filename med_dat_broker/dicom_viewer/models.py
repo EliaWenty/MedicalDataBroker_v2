b36b5a2cd5a4be0e95f4aa4bbf7fe35b4a4824cb
eliwe <eliwen@gmx.at>
2020-01-18 19:50:22 +0100

    dicom Model

--- med_dat_broker/dicom_viewer/models.py
+++ med_dat_broker/dicom_viewer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+class dcmModel(models.Model):
+    d_uuid = models.AutoField(primary_key=True)
+    d_patientuid = models.CharField(max_length=300)
+    d_seriesuid = models.CharField(max_length=300)
+    d_studyuid = models.CharField(max_length=300)
+    d_imageuid = models.CharField(max_length=300)
+    d_sopinstanceuid = models.CharField(max_length=300)
