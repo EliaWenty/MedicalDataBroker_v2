a6637bfd970d3092d9ad58f5e5eb4978bea126af
Alfons <mue17594@spengergasse.at>
2020-03-16 18:32:51 +0100

    render_pdf flush

--- med_dat_broker/ekg_viewer/utils.py
+++ med_dat_broker/ekg_viewer/utils.py
@@ -4,11 +4,16 @@ from django.template.loader import get_template
 
 from xhtml2pdf import pisa
 
+
 def render_to_pdf(template_src, context_dict={}):
+    result = BytesIO()
+    result.flush()
     template = get_template(template_src)
     html = template.render(context_dict)
     result = BytesIO()
     pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    r = result
+    result.flush()
     if not pdf.err:
-        return HttpResponse(result.getvalue(), content_type='application/pdf')
+        return HttpResponse(r.getvalue(), content_type='application/pdf')
     return None
